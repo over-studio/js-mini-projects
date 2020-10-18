405a00162dea96556695600601bdcc59fbc5e243
jem_moussa <jem_moussa@live.fr>
2020-10-18 17:52:39 -0400

    01 - JavaScript Drum Kit - UPDATED

--- 0. JavaScript30/01 - JavaScript Drum Kit/app.js
+++ 0. JavaScript30/01 - JavaScript Drum Kit/app.js
@@ -1,10 +1,10 @@
 window.addEventListener('keydown', function (e) {
-	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) || document.querySelector(`audio[data-key="${e.which}"]`);
 	if (!audio) return;
 	audio.currentTime = 0;
 	audio.play();
 
-	const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	const key = document.querySelector(`div[data-key="${e.keyCode}"]`) || document.querySelector(`div[data-key="${e.which}"]`);
 	key.classList.add('playing');
 });
 
